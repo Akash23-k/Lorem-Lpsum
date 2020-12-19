b3cc3c6c69557d32586bbf118b9241a62e37e820
Akash23-k <khanapurkar.akash23@gmail.com>
2020-12-19 18:09:14 +0530

    first commit

---
+++ src/Lorem.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import './LoremStyle.css';
+
+
+const Lorem = () => {
+        return ( <div className="container"> 
+         
+
+        
+
+                <h1 className="vl" >  &nbsp;Healthy Fruits</h1>
+                <div className="container1">
+                        <div className="inner1">
+                                <h1> Lorem Lpsum</h1>
+                                <p>Lorem lpsum is simply dummy text of the <br/> printing and type setting industry. </p>
+                        </div> 
+                </div>
+                <div className="container2">
+                        <div className="inner2">
+                                <h1> Lorem Lpsum</h1>
+                                <p>Lorem lpsum is simply dummy text of the <br/> printing and type setting industry. Lorem  <br/>lpsum has been the industry standard </p>
+                        </div> 
+                </div>
+                <div className="container3">
+                        <div className="inner3">
+                                <h1> Lorem Lpsum</h1>
+                                <p>Lorem lpsum is simply dummy text of the <br/> printing and type setting industry. </p>
+                        </div> 
+                </div>
+                <div className="page">
+                <br/>
+                <div className="title">
+                       
+                        <p className="Nutrition"> Nutrition fact</p>
+                </div>
+                        <div className="subtitle">
+                                <p className="Serving "> Serving Size <span className="content-right">100g </span>   </p>
+                                <hr/> 
+                                <p className="Amount"> Amount per serving </p>
+                                
+                        </div>
+                        <div >
+                        <p className="Calories"> Calories <span className="content-right"> 89</span> <hr/> </p>
+                        <div className="content-right-1">  %Daily value* </div>
+                        <hr className="b"/>
+        
+                       
+                        </div>
+                        
+               
+                <p> Total fat 0.3g <span className="content-right"> 0%</span>  <hr className="line"/></p>
+                <p> Saturated fat 0.1g <span className="content-right"> 0%</span> <hr className="line"/></p>
+                <p> Sodium 1g <span className="content-right"> 0%</span> <hr className="line"/> </p>
+                <p> Total carbohydrate 23g <span className="content-right"> 8%</span> <hr className="line"/> </p>
+                <p> Dietary fiber 2.6g <span className="content-right"> 9%</span> <hr className="line"/></p>
+                <p> Sugar 12g  <hr className="line"/></p>
+                <p > Protein 1.1g  <span className="content-right"> 2%</span><hr/></p>
+                <p> Vitamin D 0.00mcg <span className="content-right"> 0%</span> <hr className="line"/></p>
+                <p> Calcium 5.00mg <span className="content-right"> 0%</span><hr className="line"/> </p>
+                <p> Iron 0.26mg <span className="content-right"> 1%</span><hr className="line"/></p>
+                <p> Potassium 358mg <span className="content-right"> 8%</span><hr/> </p>
+                
+                 
+                         <li>Delivering with love and care for you to relish</li>
+
+                <div className="final">
+                <hr className="line1"/>
+                        <h3> About Demo Group </h3>
+                        <p>
+                                Leading the Crops Division for Demo Group is India's best branded fruit company and a market leader in controlled cultivation and marketing of high-quality fruits to domestic and international customers in a socially responsible manner.
+
+                        We are happy to serve fresh and delicious fruits.
+                        </p>
+                        <hr/> 
+                </div>
+                
+                </div>
+
+                </div>
+
+                
+
+    
+   
+    
+  
+                )
+            }
+        
+export default Lorem;
